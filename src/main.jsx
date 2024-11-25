f47108e0b669d214c5e4ea79e3a5ea2089e74036
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-25 10:21:23 +0600

    client site server complete module

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+
+import './index.css'
+import App from './App.jsx'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Users from './components/Users.jsx';
+import Update from './components/Update.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: '/users',
+    element: <Users />,
+    loader: () => fetch('http://localhost:5000/users')
+  },
+  {
+    path: '/update/:id',
+    element: <Update></Update>,
+    loader: ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
